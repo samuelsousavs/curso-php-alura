b7ca56f76acf32cabd5482022fabce75c63637af
Samuel de Sousa Carneiro <mueldeizzz@gmail.com>
2026-06-04 18:49:53 -0300

    condições adicionadas

--- aulas/screen-match.php
+++ aulas/screen-match.php
@@ -1,14 +1,21 @@
 <?php
-    echo 'Bem vindo(a) ao screen Match!';
-    $nomeFilme = "\nTop Gun - Maverick"; 
-    $anoLancamento = 2022;
-    $notaFilme = 8.8;
-    $incluidoNoPlano = True; 
-    echo $nomeFilme; 
+$nome = $argv[1] ?? 2022;
+echo 'Bem vindo(a) ao screen Match!';
+$nomeFilme = "\nTop Gun - Maverick"; 
+$anoLancamento = 2021;
+$notaFilme = 8.8;
+$incluidoNoPlano = True; 
+echo $nomeFilme; 
 
-    echo "\nNota do Filme: " . $notaFilme; 
-    echo "\nNota do filme: $notaFilme"
-    // Com aspas simples ele nao interpreta nada
-    // Com aspas duplas ele interpreta
-    
+echo "\nNota do Filme: " . $notaFilme; 
+echo "\nNota do filme: $notaFilme";
+
+if ($anoLancamento > 2022) {
+    echo "\nEsse filme é um lançamento";
+} elseif($anoLancamento > 2020 && $anoLancamento < 2022) {
+    echo "\nEsse filme é Iconico!";
+} else {
+    echo "\nEsse filme não é um lançamento";
+}
+
 ?>  
