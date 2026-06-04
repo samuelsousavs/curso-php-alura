066af12046f45587f7e76f7ef07524264cc7adba
Samuel de Sousa Carneiro <mueldeizzz@gmail.com>
2026-06-04 15:38:38 -0300

    Segunda Aula

---
+++ screen-match.php
@@ -0,0 +1,12 @@
+<?php
+    echo 'Bem vindo(a) ao screen Match!
+    ';
+    $nomeFilme = 'Top Gun - Maverick'; 
+    // Sintaxe da Atribuição de uma Variavel
+    $anoLancamento = 2022;
+    $notaFilme = 8.8;
+    $incluidoNoPlano = True; 
+    echo $nomeFilme; 
+
+
+?>
