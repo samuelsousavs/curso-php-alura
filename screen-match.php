9c69a868145dda302cdd347e317125a454571188
Samuel de Sousa Carneiro <mueldeizzz@gmail.com>
2026-06-04 15:42:25 -0300

    Operadores logicos

--- screen-match.php
+++ screen-match.php
@@ -1,12 +1,19 @@
 <?php
     echo 'Bem vindo(a) ao screen Match!
     ';
-    $nomeFilme = 'Top Gun - Maverick'; 
+    $nomeFilme = 'Top Gun - Maverick
+    '; 
     // Sintaxe da Atribuição de uma Variavel
     $anoLancamento = 2022;
     $notaFilme = 8.8;
     $incluidoNoPlano = True; 
     echo $nomeFilme; 
 
+    // Operadores Logicos 
+    $nota1 = 2;
+    $nota2 = 33; 
+    $nota3 = 12; 
+    $mediaNotas = ($nota1 + $nota2 + $nota3) / 3;
+    echo 'Media das Notas: ' . $mediaNotas; 
 
 ?>
